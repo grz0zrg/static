8fe55abe174a81bf5a590a954fc3cbe14a9b81e2
Julien Verneuil <grz.zrg@gmail.com>
2018-07-29 15:42:04 +0200

    initial

---
+++ README.md
@@ -0,0 +1,149 @@
+# Minimalist Static Website Tool
+This Python 2.x tool was built to produce my portfolio at : http://www.onirom.fr
+
+You can find a complete usage example here : https://github.com/grz0zrg/portfolio
+
+It provide a simple way to produce static HTML pages content from a combination of JSON data definitions and HTML templates, tags related to JSON definition in the form of `{tag}` can be used inside the template files.
+
+While it lack many features, this tool work great if your website has a simple structure with a navigation menu and pages listing items, ideal as a portfolio generator.
+
+### Example
+
+Directory structure (folders) :
+
+* json
+  * nav.json
+  * items.json
+* template
+  * nav.html
+  * items.html
+* dist
+  * intex.html
+
+
+
+#### nav.json
+
+This list all the pages to be generated, in this case a `software.html` and `library.html` will be produced, page name with space character are converted automatically into a page name with space replaced by an underscore.
+
+```json
+{
+    "pages": [
+        {
+            "name": "software"
+        },
+        {
+            "name": "library"
+        }
+    ]
+}
+```
+
+#### nav.html
+
+The template file linked to `nav.json` which will generate a list of items, allowed tags :
+
+* `name`
+  * The page name
+* `_name`
+  * The page name as it was defined in the `nav.json` file (without conversions)
+* `active`
+  * This will be replaced by the string `active` if this item correspond to the generated page (so it can be highlighted as current page with a CSS class or something)
+
+```html
+<li class="item {active}">
+    <a href="{name}.html" class="nav-link">
+        {_name}
+    </a>
+</li>
+```
+
+#### items.json
+
+This list all available definitions for each pages, all properties listed here can be used inside the `items.html` template file, you can use an array as a value so that each items are enclosed within a div (great for styling a list of items like tags)
+
+```json
+{
+    "software": [
+        {
+            "thumb_url": "assets/thumb/my_software.png",
+            "thumb_alt": "Software example",
+            "link": "https://mysoftware.com",
+            "thumb_alt": "Software example",
+            "title": "Software example",
+            "tags": ["C", "Embedded systems", "REST", "JSON"]
+        }
+    ],
+    "library": [
+        {
+            "thumb_url": "assets/thumb/mylibrary.png",
+            "thumb_alt": "Library example",
+            "link": "https://mylibrary.com",
+            "thumb_alt": "Library example",
+            "title": "Library example",
+            "tags": ["C/C++", "Linux"]
+        }
+    ]
+}
+```
+
+#### items.html
+
+The items template file linked to `items.json`
+
+```html
+<li class="item">
+    <a href="{link}" title="{link}" target="_blank" rel="noopener">
+        <img class="item-thumb" src="{thumb_url}" alt="{thumb_alt}"/>
+        <div class="item-label">
+            <span class="item-title">
+                {title}
+            </span>
+            <div class="item-tags">
+                <div class="item-tag">
+                    {tags}
+                </div>
+            </div>
+        </div>
+    </a>
+</li>
+```
+
+#### index.html
+
+Your HTML skeleton, all pages will be generated from this page, you can use a tag of the same name as the JSON files to specify where the generated items goes such as `{nav}` and `{items}` in this example.
+
+You can also use the tag `{date}` to place the generation date.
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+    <body>
+        <main class="content">
+            <div class="content-nav">
+                <ul class="items">
+                    {nav}
+                </ul>
+            </div>
+            <div class="content-items">
+                <ul class="items">
+                    {items}
+                </ul>
+            </div>
+        </main>
+        
+        <footer>
+            <div class="update">
+                generated on {date}
+            </div>
+        </footer>
+    </body>
+</html>
+    </body>
+```
+
+Then you call the tool : `python static_build.py json/nav.json json/items.json dist/index.html`
+
+All generated pages can found in the `dist` folder.
---
+++ static_build.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#
+# Minimalist static site builder to be used with https://github.com/grz0zrg/pynut (not needed but may be useful to concat JS/CSS/HTML file into a single file!)
+#
+# This tool was built to produce my portfolio at : www.onirom.fr
+# For an usage example : https://github.com/grz0zrg/portfolio
+#
+# It provide a simple way to produce generated HTML pages content from a combination of JSON data definitions and HTML templates.
+#
+# The first argument is a JSON file (which should be placed in a 'json' directory) which list all the pages to be generated, it may come with a HTML template to build a navigation menu for example.
+# The second argument is a JSON file (which should be placed in a 'json' directory) which list all the specific content attached to a page, all JSON files are linked to a HTML template file of the same name.
+# The last argument is the regular HTML skeleton page with tags indicating where to place the navigation menu and content.
+#
+# Usage: static_build.py json/categories.json json/items.json dist/index.html
+
+import os
+import re
+import sys
+import glob
+import json
+import codecs
+import datetime
+
+def gen_nav_menu(html_template_file, pages):
+    with open(html_template_file, 'r') as content_file:
+        html_template = content_file.read()
+
+    pages_nav = []
+
+    for current_page in pages:
+        html_output = ""
+
+        current_page_name = current_page["name"].replace(" ", "_")
+
+        for item in pages:
+            temp_html = ""
+
+            page_name_org = item["name"]
+            page_name = page_name_org.replace(" ", "_")
+
+            if page_name == current_page_name:
+                temp_html += html_template.replace("{active}", "active")
+            else:
+                temp_html += html_template.replace("{active}", "")
+
+            html_output += temp_html.replace("{name}", page_name).replace("{_name}", page_name_org)
+
+        pages_nav.append(html_output)
+
+    return pages_nav
+
+def gen_content(html_template_file, pages, content_json):
+    with open(html_template_file, 'r') as content_file:
+        html_template = content_file.read()
+
+    contents = []
+
+    dict_tmp = {}
+
+    for key in content_json:
+        dict_tmp[key.replace(" ", "_")] = content_json[key]
+
+    content_json = dict_tmp
+
+    for current_page in pages:
+        html_output = ""
+
+        content_obj = content_json[current_page["name"].replace(" ", "_")]
+
+        for content in content_obj:
+            html_temp = html_template
+            
+            for key in content:
+                if type(content[key]) is list:
+                    tmp_content = ''.join('<div>{0}</div>'.format(w) for w in content[key])
+                else:
+                    tmp_content = content[key]
+                html_temp = html_temp.replace("{" + key + "}", tmp_content)
+
+            html_output += html_temp
+
+        contents.append(html_output)
+    
+    return contents
+
+work_directory = os.getcwd()
+
+if len(sys.argv) <= 3:
+    print("Usage example: python static_build.py categories.json items.json dist/index.html")
+else:
+    print("\n Static site builder :\n")
+
+    json_pages = sys.argv[1]
+    json_content = sys.argv[2]
+    html_file = sys.argv[3]
+
+    (json_pages_name, tmp_ext) = os.path.splitext(json_pages)
+    
+    pages_nav_html_arr = []
+    contents_html_arr = []
+
+    html_template_file = "template/" + json_pages_name + ".html"
+
+    # read pages; JSON file
+    with open("json/" + json_pages) as data_file:    
+        data = json.load(data_file)
+
+    pages = data["pages"]
+
+    print("   Generating nav. data...")
+
+    # generate HTML nav. data per pages
+    if os.path.isfile(html_template_file):
+        pages_nav_html_arr = gen_nav_menu(html_template_file, pages)
+    
+    # read content; JSON file
+    with open("json/" + json_content) as data_file:    
+        data = json.load(data_file)
+
+    (json_content_name, tmp_ext) = os.path.splitext(json_content)
+
+    html_template_file = "template/" + json_content_name + ".html"
+
+    print("   Generating content data...\n")
+
+    # generate HTML content data per pages
+    if os.path.isfile(html_template_file):
+        contents_html_arr = gen_content(html_template_file, pages, data)
+    
+    # read HTML skeleton
+    content = ""
+
+    with codecs.open(filename=html_file, mode='r', encoding='UTF-8') as f:
+        for line in f:
+            content += line
+        f.close()
+
+    # search & replace date tag
+    today = datetime.date.today()
+    today_formatted = today.strftime('%b %d, %Y')
+
+    content = content.replace("{date}", today_formatted)
+
+    index = 0
+    
+    # construct all HTML pages from the informations gathered
+    for page_name in pages:
+        page_name = page_name["name"].replace(" ", "_")
+
+        page_filename = "dist/" + page_name + ".html"
+
+        print("   Constructing page '" + page_filename + "'")
+
+        temp_content = content
+
+        temp_content = temp_content.replace("{" + json_pages_name + "}", pages_nav_html_arr[index])
+        temp_content = temp_content.replace("{" + json_content_name + "}", contents_html_arr[index])
+
+        f = codecs.open(filename=page_filename, mode='w', encoding='UTF-8')
+        f.write(temp_content)
+        f.close()
+
+        index += 1
+
+    print("\n Done.")
